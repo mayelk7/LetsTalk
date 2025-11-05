238e0d0920134a60b75809cdb697411d9f875fa8
KERIVEL Mael <mael.kerivel@azylis.net>
2025-11-05 15:35:18 +0100

    . ajout de l'exemple de classe pour ajouter a la base

---
+++ LetsTalk/LetsTalk.Client/Models/ClassExemple.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LetsTalk.Client.Models
+{
+    [PrimaryKey (nameof(Id))]
+    [Index(nameof(Name), IsUnique = true)]
+    public class ClassExemple
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; }
+    }
+}
--- LetsTalk/LetsTalk/Context/AppDbContext.cs
+++ LetsTalk/LetsTalk/Context/AppDbContext.cs
@@ -5,4 +5,5 @@ namespace LetsTalk.Context;
 public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
 {
     // public DbSet<Model> Models { get; set; }
+    public DbSet<LetsTalk.Client.Models.ClassExemple> ClassExemples { get; set; }
 }
