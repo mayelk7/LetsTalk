a84f00f44b346f3702ab6a1e9defa0f956ccfd1e
= <=>
2026-01-14 09:41:56 +0100

    Attention j'envoie Lubin
    Refactorisation du hachage des mots de passe utilisateur
    
    Introduction de PasswordHelper basé sur PasswordHasher d'ASP.NET Identity pour centraliser et sécuriser le hachage et la vérification des mots de passe. Remplacement de l'ancienne logique PBKDF2 dans BackApiEf par l'utilisation de cette nouvelle classe utilitaire. Amélioration de la sécurité et de la maintenabilité du code.

--- LetsTalk/LetsTalk/Data/BackApiEf.cs
+++ LetsTalk/LetsTalk/Data/BackApiEf.cs
@@ -4,6 +4,8 @@ using LetsTalk.Context;
 using LetsTalk.Models;
 using LetsTalk.Shared;
 using LetsTalk.Shared.ModelsDto;
+using Microsoft.AspNetCore.Identity;
+using LetsTalk.Helpers;
 
 namespace LetsTalk.Data;
 public class BackApiEf
@@ -42,7 +44,7 @@ public class BackApiEf
             Username = username,
             Email = email,
             Phone = phone,
-            Password = Convert.ToBase64String(pbkdf2.GetBytes(32)), // Stocker la clé dérivée en Base64
+            Password = PasswordHelper.Hash(password),
             ProfilPicture = null,
             Actif = true,
             Salt = salt,
