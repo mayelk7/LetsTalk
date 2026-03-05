ca22970b1d1429e839ebdbb015324ad73b24df2f
Frédéric Carisey <63855888+fcarisey@users.noreply.github.com>
2026-03-05 10:55:28 +0100

    Refactor LivekitApiController and VoiceChannel; update context menu handling and API response structure

--- LetsTalk/LetsTalk/Controllers/LivekitApiController.cs
+++ LetsTalk/LetsTalk/Controllers/LivekitApiController.cs
@@ -4,38 +4,34 @@ using Microsoft.AspNetCore.Mvc;
 using LetsTalk.Shared.Api;
 using LetsTalk.Shared.ModelsDto;
 
-namespace LetsTalk.Controllers
+namespace LetsTalk.Controllers;
+
+[ApiController]
+[Route("api/voice")]
+public class LivekitApiController : BaseApiController
 {
+    private readonly LivekitService _livekit;
+    private readonly BackApiEf _backapi;
 
-    [ApiController]
-    [Route("api/voice")]
-    public class LivekitApiController : BaseApiController
+    public LivekitApiController(LivekitService livekit, BackApiEf backapi)
     {
-        private readonly LivekitService _livekit;
-        private readonly BackApiEf _backapi;
-
-        public LivekitApiController(LivekitService livekit, BackApiEf backapi)
-        {
-            _livekit = livekit;
-            _backapi = backapi;
-        }
-
-        // GET api/voice/{roomName}/members
-        [HttpGet("{roomName}/members")]
-        public async Task<ApiResponse<List<ParticipantLiveKit>>> GetMembers(string roomName)
-        {
-            var members = await _backapi.GetMembersWithDbInfo(roomName);
-            return BaseApiController.Response<List<ParticipantLiveKit>>("Members retrieved successfully", members);
-        }
-
-        // GET api/voice/rooms
-        [HttpGet("rooms")]
-        public async Task<IActionResult> GetRooms()
-        {
-            var rooms = await _livekit.ListerSalonsActifs();
-            return Ok(rooms);
-        }
+        _livekit = livekit;
+        _backapi = backapi;
     }
 
-    
+    // GET api/voice/{roomName}/members
+    [HttpGet("{roomName}/members")]
+    public async Task<ApiResponse<List<ParticipantLiveKit>>> GetMembers(string roomName)
+    {
+        // var members = await _backapi.GetMembersWithDbInfo(roomName);
+        return BaseApiController.Response<List<ParticipantLiveKit>>("Members retrieved successfully", []);
+    }
+
+    // GET api/voice/rooms
+    [HttpGet("rooms")]
+    public async Task<IActionResult> GetRooms()
+    {
+        var rooms = await _livekit.ListerSalonsActifs();
+        return Ok(rooms);
+    }
 }
