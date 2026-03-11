270551cbad1d352b7f1f1c5467d1930d5a2a1e40
KERIVEL Mael <mael.kerivel@azylis.net>
2026-03-11 10:15:48 +0100

    ajout du JS pour envoyer le message via la touche entrée

---
+++ LetsTalk/LetsTalk.Client/wwwroot/InputTextChat.js
@@ -0,0 +1,10 @@
+﻿window.registerChatInput = (dotnetRef, elementId) => {
+    const el = document.getElementById(elementId)?.querySelector('textarea');
+    if (!el) return;
+    el.addEventListener('keydown', (e) => {
+        if (e.key === 'Enter' && !e.shiftKey) {
+            e.preventDefault();
+            dotnetRef.invokeMethodAsync('SendMessage');
+        }
+    });
+};
