01d3c7e851901497e6f4a93b8e478b64170fcd30
Luc <luc.bourguignon2005@gmail.com>
2026-05-11 09:48:39 +0200

    Enhance ApiManagerService and database models to support file uploads in messages; update related API controllers and view models

---
+++ LetsTalk/LetsTalk.Shared/ModelsDto/AssignRoleRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetsTalk.Shared.ModelsDto;
+
+public class AssignRoleRequest
+{
+    public int RoleId { get; set; }
+}
